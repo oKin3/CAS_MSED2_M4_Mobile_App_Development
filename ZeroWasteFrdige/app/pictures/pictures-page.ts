1cdca98d8281c0fa4ee3b1c1507c28c75858ee66
urivero <149521311+urivero@users.noreply.github.com>
2024-01-07 20:16:53 +0100

    app can use the camera after clicking button in pictures

--- ZeroWasteFrdige/app/pictures/pictures-page.ts
+++ ZeroWasteFrdige/app/pictures/pictures-page.ts
@@ -1,10 +1,16 @@
-import { Dialogs } from '@nativescript/core'
+import { Dialogs, TouchAction } from '@nativescript/core'
 import { NavigatedData, Page } from '@nativescript/core'
 import { PictureViewModel } from './picture-view-model'
+import { takePicture } from '@nativescript/camera'
+import { ImageSource } from '@nativescript/core/image-source'
+import { knownFolders } from '@nativescript/core'
+
+const appRootFolder = knownFolders.currentApp()
 
 export function onNavigatingTo(args: NavigatedData) {
   const page = <Page>args.object
   page.bindingContext = new PictureViewModel()
+  console.log(appRootFolder.path)
 }
 
 export function handleTakePicture() {
@@ -12,11 +18,17 @@ export function handleTakePicture() {
         title: 'Do you want to take a picture?',
         message: 'Are you sure you want to do this?',
         okButtonText: 'Yes',
-        cancelButtonText: 'No',
-        neutralButtonText: 'Cancel',
+        cancelButtonText: 'No'
       }).then((result) => {
-        console.log(result)
-      })
-}
-
+        if(result) {
+            console.log('user is sure')
+            var milliseconds = (new Date).getTime
+            takePicture({width:300, height:300, keepAspectRatio:true}).then((function(img) {
+                let source = new ImageSource()
+                source.saveToFileAsync(appRootFolder.path + milliseconds , "png")
+                }))
+            }
+        })
+    }
+      
 
